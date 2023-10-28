62ef3483c88f520c80f5cd01a9cd409c38faf14e
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-10-28 14:05:10 -0300

    exercício alterando Innerhtml

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function turnOn() {
-    document.getElementById("imgLightOff").style.display='none'
-    document.getElementById("imgLightOn").style.display='block'
-   
+    document.getElementById("imgLightOff").style.display = 'none'
+    document.getElementById("imgLightOn").style.display = 'block'
+
 }
 
 
 function turnOff() {
-    document.getElementById("imgLightOff").style.display='block'
-    document.getElementById("imgLightOn").style.display='none'
-   
+    document.getElementById("imgLightOff").style.display = 'block'
+    document.getElementById("imgLightOn").style.display = 'none'
+
 
 }
 
+
+function changeP() {
+    document.getElementById("pTest").innerHTML = 'I have changed'
+}
