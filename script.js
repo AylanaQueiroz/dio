ca5acae32fb6a1b733377c8fe6334bdd06b32a15
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-10-30 12:03:06 -0300

    exercisio js - Radar de velocidades

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function turnOff() {
 function changeP() {
     document.getElementById("pTest").innerHTML = 'I have changed'
 }
+
+
+function testeIn(){
+    document.getElementById("testeInner").innerHTML="mudar"
+}
+
+
+
+let velAtingida = document.getElementById("velCarro");
+
+const velocidadeMaxima = 80;
+let resultado = document.getElementById("resultadoVelocidade");
+
+function radarDeVelocidade(){
+    
+    
+    if (velAtingida.value >= velocidadeMaxima) {
+        resultado.innerHTML = "VOCÊ FOI MULTADO"
+    }    else resultado.innerHTML ="Pode prosseguir com segurança"
+
+}
+
+//
