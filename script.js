7d979ae83787735af03e43d6fc95862b89eab695
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-11-01 17:15:38 -0300

    exercisios js - ligar desligar

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function changeP() {
 }
 
 
-function testeIn(){
-    document.getElementById("testeInner").innerHTML="mudar"
+function testeIn() {
+    document.getElementById("testeInner").innerHTML = "mudar"
 }
 
 
@@ -29,12 +29,12 @@ let velAtingida = document.getElementById("velCarro");
 const velocidadeMaxima = 80;
 let resultado = document.getElementById("resultadoVelocidade");
 
-function radarDeVelocidade(){
-    
-    
+function radarDeVelocidade() {
+
+
     if (velAtingida.value >= velocidadeMaxima) {
         resultado.innerHTML = "VOCÊ FOI MULTADO"
-    }    else resultado.innerHTML ="Pode prosseguir com segurança"
+    } else resultado.innerHTML = "Pode prosseguir com segurança"
 
 }
 
@@ -43,8 +43,43 @@ const teste = document.getElementById('gtest')
 console.log(teste)
 
 
-const lampadaLigada =document.querySelector('#lampadaOnTeste');
+const lampadaLigada = document.querySelector('#lampadaOnTeste');
 
-function desligarTeste(){
-    lampadaLigada.src="./img/lightOff.jpg"
+function desligarTeste() {
+    lampadaLigada.src = "./img/lightOff.jpg"
 }
+
+//exercício luz verde e luz vermelha utilizando o addEventListenner
+
+/*var green = document.querySelector('#testGreen');
+
+function changeImg() {
+    green.src="./img/redButton.avif"
+}
+
+green.addEventListener('click',changeImg)*/
+
+
+var botaoOff = document.querySelector('#btnOff');
+var green = document.querySelector('#testGreen');
+var botaoON = document.querySelector('#btnON');
+var red = document.querySelector('#testRed')
+
+
+
+function changeImgToRed() {
+    // botaoOff.src="./img/redButton.avif"
+    green.src = "./img/redButton.avif"
+
+}
+
+botaoOff.addEventListener('click', changeImgToRed)
+
+
+
+function changeImgToGreen() {
+    red.src = "./img/greenButton.avif"
+    //green.src = "./img/redButton.avif"
+}
+
+botaoON.addEventListener('click', changeImgToGreen)
