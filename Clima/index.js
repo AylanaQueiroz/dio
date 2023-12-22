7dd50cc008eb61a514282b95293fa350dcfea784
AylanaQueiroz <aylanaqueiroz@gmail.com>
2023-12-21 21:05:00 -0300

    Consumindo Api para verificar o clima - entender como acessar os dados da api

--- Clima/index.js
+++ Clima/index.js
@@ -1,14 +1,24 @@
 const pesquisar = document.querySelector('#botaoBuscar')
+const cidade = document.querySelector('.inputCidade')
+const divBody = document.querySelector('.climaAqui')
+
 
 pesquisar.addEventListener('click', informarClima)
 
+
+
 async function informarClima() {
 
-    const chave = '46bbf8fe32bbd679e3c3ef640f7f251f';
-    const cidade = 'goiania'
-    
 
-    const climaAgora = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}`)
+
+    const chave = '46bbf8fe32bbd679e3c3ef640f7f251f';
+    const cidade = document.querySelector('.inputCidade')
+    const idioma = 'pt_br'
+    const buscarCidade = cidade.value
+
+    console.log(buscarCidade)
+
+    const climaAgora = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${buscarCidade}&appid=${chave}&lang=${idioma}`)
 
     console.log(climaAgora)
 
@@ -16,12 +26,17 @@ async function informarClima() {
 
     console.log(data)
 
+    const novaDiv = document.createElement('div')
+    divBody.appendChild(novaDiv)
     
- 
+    const novoParagrafo = document.createElement('p')
+    novoParagrafo.innerText =('A temperatura a minha cidade é '   )
+    divBody.appendChild(novoParagrafo)
+    
+    
+    
+
 }
 
 informarClima()
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${appkey}
